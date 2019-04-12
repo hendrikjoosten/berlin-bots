f7dbfdbd6d07f325085f210bcfb293c3c64d7118
Unknown <andrey.bogomolov@tryggel.com>
2019-04-12 23:06:06 +0200

    Smarter frontend

--- app/hello.py
+++ app/hello.py
@@ -1,6 +1,18 @@
 from flask import Flask
+
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello World!"
+def route_transit():
+    return '<h1>Hello World</h1>'
+
+
+@app.route("/test")
+def func1():
+    return '<h1>Hello World on test</h1>'
+
+
+app.run(debug=True, host='localhost', port=80)
+
+
